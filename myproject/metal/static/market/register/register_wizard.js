bcbbafe6703176704f7fbe5b896e30a8504a86a0
alfeosilvestro <alfeo.silvestro@live.com>
2017-10-12 18:46:24 +0800

    registration logic

--- myproject/metal/static/market/register/register_wizard.js
+++ myproject/metal/static/market/register/register_wizard.js
@@ -3,16 +3,14 @@ $(document).ready(function () {
     hideSupplierSteps();
 
     var chkRegAsSup = $('#chkRegisterAsSupplier');
-    chkRegAsSup.on('click', function(e){
+    chkRegAsSup.on('click', function (e) {
         var isChecked = chkRegAsSup.prop('checked');
         isRegisterAsSupplier = isChecked;
 
-        if(isRegisterAsSupplier)
-        {
+        if (isRegisterAsSupplier) {
             showAllSteps();
         }
-        else
-        {
+        else {
             hideSupplierSteps();
         }
     });
@@ -34,8 +32,7 @@ $(document).ready(function () {
 
         var $active = $('.wizard .nav-tabs li.active');
 
-        if(isRegisterAsSupplier)
-        {
+        if (isRegisterAsSupplier) {
             $active.next().removeClass('disabled');
             nextTab($active);
         }
@@ -44,19 +41,15 @@ $(document).ready(function () {
             lastTab($active);
         }
 
-        console.log($(".select2").val());
-
     });
     $(".prev-step").click(function (e) {
 
         var $active = $('.wizard .nav-tabs li.active');
 
-        if(isRegisterAsSupplier)
-        {
+        if (isRegisterAsSupplier) {
             prevTab($active);
         }
-        else
-        {
+        else {
             $active.parent().children().first().removeClass('disabled');
             first($active);
         }
@@ -64,13 +57,13 @@ $(document).ready(function () {
     });
 
     // Get Service
-    $(document).on('click', '.chk_service', function (){
-    //$('.chk_service').on('click', function(event) {
+    $(document).on('click', '.chk_service', function () {
+        //$('.chk_service').on('click', function(event) {
         update_Next_Level_Services($(this));
     });
 
     // Submit post on submit
-    $('#btnSubmit').on('click', function(event){
+    $('.btnSubmit').on('click', function (event) {
         event.preventDefault();
         console.log("form submitted!")  // sanity check
         register_user($(this));
@@ -87,16 +80,20 @@ function hideSupplierSteps() {
     $('#btnNext').hide();
     $('#btnSubmit').show();
 
+    $('#selTags').hide();
+
     $('.wizard .wizard-inner .nav-tabs li').css('width', '50%');
 }
 
-function showAllSteps(){
+function showAllSteps() {
 
     $('#iconStep2').show();
     $('#iconStep3').show();
     $('#iconStep4').show();
     $('#btnNext').show();
     $('#btnSubmit').hide();
+
+    $('#selTags').show();
 
     $('.wizard .nav-tabs li').css('width', '20%');
 
@@ -126,21 +123,26 @@ function register_user(form) {
     console.log("create post is working!") // sanity check
 
     // Collect Data
+    var data = collect_form_data()
+    console.log(data);
+
+    // remove after testing data collection
+    return false;
 
     var frmData = {
         csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(), // essential data
-        the_post : $('#post-text').val()
+        the_post: $('#post-text').val()
     };
 
     // Save
 
     $.ajax({
-        url : form.prop('action'), // the endpoint
-        type : "POST", // http method
-        data : frmData, // data sent with the post request
+        url: form.prop('action'), // the endpoint
+        type: "POST", // http method
+        data: frmData, // data sent with the post request
 
         // handle a successful response
-        success : function(json) {
+        success: function (json) {
             // remove the value from the input
 
             // move to last anchor
@@ -152,8 +154,8 @@ function register_user(form) {
         },
 
         // handle a non-successful response
-        error : function(xhr,errmsg,err) {
-            $('#results').html("<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: "+errmsg+
+        error: function (xhr, errmsg, err) {
+            $('#results').html("<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: " + errmsg +
                 " <a href='#' class='close'>&times;</a></div>"); // add the error to the dom
             console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
         }
@@ -162,7 +164,7 @@ function register_user(form) {
 
 //  get service by id
 //  1
-function update_Next_Level_Services(source_checkbox){
+function update_Next_Level_Services(source_checkbox) {
     var ischecked = source_checkbox.prop('checked');
     var serviceId = source_checkbox.val();
     var renderin = source_checkbox.data('renderin');
@@ -170,9 +172,9 @@ function update_Next_Level_Services(source_checkbox){
     var thisText = source_checkbox.data('thistext');
     var thisStep = source_checkbox.data('step');
 
-    if(thisStep == 4) return false; // if final step do not do ajax call
+    if (thisStep == 4) return false; // if final step do not do ajax call
 
-    if(ischecked) {
+    if (ischecked) {
         // load data and upload
         $.ajax({
             url: '/supplier/service/' + serviceId, // the endpoint
@@ -194,7 +196,7 @@ function update_Next_Level_Services(source_checkbox){
                 console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
             }
         });
-    }else{
+    } else {
         // remove next level
         $('.child-of-' + serviceId).detach();
     }
@@ -206,16 +208,16 @@ function update_destination_element(elementId, json, parentText, thisText, thisS
     var nextStep = thisStep + 1;
 
     var textSeparator = '';
-    if(parentText != ''){
+    if (parentText != '') {
         textSeparator = ' > ';
     }
 
     var targetElement = $('#' + elementId);
     //targetElement.empty();
 
-    var containerOpening = '<div class="box box-solid box-default child-of-'+ serviceId +'">\n' +
+    var containerOpening = '<div class="box box-solid box-default child-of-' + serviceId + '">\n' +
         '            <div class="box-header with-border">\n' +
-        '              <h3 class="box-title">'+ parentText + textSeparator + thisText +'</h3>\n' +
+        '              <h3 class="box-title">' + parentText + textSeparator + thisText + '</h3>\n' +
         '            </div>\n' +
         '            <!-- /.box-header -->\n' +
         '            <div class="box-body">';
@@ -227,18 +229,18 @@ function update_destination_element(elementId, json, parentText, thisText, thisS
     var closingElement = '</label></div>';
     var finalElment = containerOpening;
 
-    json.forEach(function(element) {
+    json.forEach(function (element) {
         // console.log(element.Id);
         // console.log(element.Service_Name);
 
         var checkboxElement = '<input ' +
             'class="chk_service" ' +
-            'data-thistext="'+ element.Service_Name +'" ' +
-            'data-parenttext="'+ thisText +'" ' +
+            'data-thistext="' + element.Service_Name + '" ' +
+            'data-parenttext="' + thisText + '" ' +
             'data-renderin="step4_list" ' +
-            'data-step="'+ nextStep +'" ' +
+            'data-step="' + nextStep + '" ' +
             'type="checkbox" ' +
-            'value="'+ element.Id +'" /> ' +
+            'value="' + element.Id + '" /> ' +
             element.Service_Name;
 
         finalElment = finalElment + openingElement + checkboxElement + closingElement;
@@ -251,3 +253,83 @@ function update_destination_element(elementId, json, parentText, thisText, thisS
 
 }
 
+function collect_form_data() {
+
+    var data = {};
+
+    data.company_uen = ''; // txt_company_uen
+    data.company_name = ''; // txt_company_name
+    data.contact_number = ''; // txt_contact_number
+    data.tags = $(".select2").val(); //
+    data.user_name = ''; // txt_user_name
+    data.job_title = ''; // txt_job_title
+    data.email_address = ''; // txt_email_addr
+    data.password = ''; // txt_password
+    data.services = []; //
+    data.register_as_supplier = $('#chkRegisterAsSupplier').prop('checked');
+
+    var step4_checkboxes = $('*[data-step="4"]');
+
+    step4_checkboxes.each(function (i, element) {
+
+        if (element.checked) {
+            data.services.push(element.value);
+        }
+
+    });
+
+    return data;
+
+}
+
+function validate_form() {
+    $("form[name='registration']").validate()
+}
+
+function register_form_validation(){
+    $("form[name='registration']").validate({
+        // Specify validation rules
+        rules: {
+            // The key name on the left side is the name attribute
+            // of an input field. Validation rules are defined
+            // on the right side
+            company_uen: "required",
+            company_name: "required",
+            contact_number: "required",
+            user_name: "required",
+            title: "required",
+            user_name: "required",
+            title: "required",
+            email_address: {
+                required: true,
+                // Specify that email should be validated
+                // by the built-in "email" rule
+                email: true
+            },
+            password: {
+                required: true,
+                minlength: 5
+            }
+        },
+        // Specify validation error messages
+        messages: {
+            company_uen: "required",
+            company_name: "required",
+            contact_number: "required",
+            user_name: "required",
+            title: "required",
+            user_name: "required",
+            title: "required",
+            password: {
+                required: "required",
+                minlength: "Your password must be at least 5 characters long"
+            },
+            email: "Please enter a valid email address"
+        },
+        // Make sure the form is submitted to the destination defined
+        // in the "action" attribute of the form when valid
+        submitHandler: function (form) {
+            form.submit();
+        }
+    });
+}
